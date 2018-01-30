aa41f2753ccf35b733d73fb9dfc5117dfaa7c17c
Simon Neumair <neunomis@web.de>
2018-01-30 17:10:40 +0100

    updated test for new State behaviour

--- test/InterpreterSpec.hs
+++ test/InterpreterSpec.hs
@@ -2,20 +2,36 @@
 module InterpreterSpec(spec) where
 
 import Lib
+import YMLReader
 import DataDefinitions
 import Test.Hspec
 import Test.QuickCheck
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+emptyState = State Set.empty Set.empty [] Map.empty Map.empty
 
 spec :: Spec
 spec =
   describe "process" $ do
-    it "dummy" $
-      1 `shouldBe` 1
---    it "processes Events" $
---      (process State [Read_Output]) `shouldBe` State
---    it "collects missing and available variables" $
---      (process State [Add_File (Add_Relation "name1" "name2")]) `shouldBe` State ["name1"] ["name2"]
---    it "collects multiple missing variables" $
---      (process State [Add_File (Add_Relation "var1" "out1"),Add_File (EDRL_File "var2" "out2")]) `shouldBe` State (Missing_Vars ["var2","var1"]) (Available_Vars ["out2","out1"])
---    it "deletes missing variables once provided" $
---      (process Empty_State [Add_File (EDRL_File "var1" "out1"),Add_File (EDRL_File "out1" "out2")]) `shouldBe` Filled_State (Missing_Vars ["var1"]) (Available_Vars ["out2","out1"])
+    it "processes Events" $
+      (process emptyState [Read_Output]) `shouldBe` emptyState
+    it "collects missing and available variables" $
+      (process emptyState [
+      Add_Relation(Relation ["input1"] "output1" ["rule1"])
+      ]) `shouldBe`
+      emptyState {missing = Set.singleton "input1" , available = Set.singleton "output1"}
+    it "collects multiple missing variables" $
+      (process emptyState [
+      Add_Relation(Relation ["input1"] "output1" ["rule1"])
+      ,Add_Relation(Relation ["input2"] "output2" ["rule2"])
+      ]) `shouldBe`
+      emptyState {missing = Set.fromList ["input1","input2"] , available =Set.fromList ["output1","output2"]}
+    it "deletes missing variables once provided" $
+      (process emptyState [
+      Add_Relation(Relation ["input1"] "output1" ["rule1"])
+      ,Add_Relation(Relation ["output1"] "output2" ["rule2"])
+      ]) `shouldBe`
+      emptyState {missing = Set.fromList ["input1"] , available =Set.fromList ["output1","output2"]}
